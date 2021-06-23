82542332596faf049d4dce3d86b2da8a4bf35d08
Pierre Borlagdan <borlagdanpierre@gmail.com>
2021-06-23 16:02:44 +0800

    Added wrapped layout css module for whole content

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,12 @@ export default function signUp() {
   return (
     <div className={formStyles.form-container}>
       <h1 className={formStyles.heading-text}>Sign Up</h1>
-
+      <form className={formStyles.sign-form}>
+        <p className={formStyles.form-text}>Username: </p><input type="text" className={formStyles.input-field} placeholder="Enter username here..."/>
+        <p className={formStyles.form-text}>Username: </p><input type="text" className={formStyles.input-field} placeholder="Enter username here..."/>
+        <p className={formStyles.form-text}>Username: </p><input type="text" className={formStyles.input-field} placeholder="Enter username here..."/>
+        <input type="submit" value="Submit"/>
+      </form>
     </div>
   )
 }
