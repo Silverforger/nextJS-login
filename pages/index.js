e1424e0546772e5b5ce7406ae6427faf761ab5cd
Pierre Borlagdan <borlagdanpierre@gmail.com>
2021-06-23 16:57:15 +0800

    First attempt at using a static json file for fetch requests upon signup and signin

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import Head from 'next/head'
+import { useState, useEffect } from 'react'
 import formStyles from '../styles/Form.module.css'
 
-export default function signUp() {
+export default function signUp({ accounts }) {
+  const onSignUp = () => {
+    console.log(accounts)
+  }
+
   return (
     <div className={formStyles.center}>
       <div className={formStyles.formcontainer}>
@@ -10,9 +15,20 @@ export default function signUp() {
           <p className={formStyles.formtext}>Username: </p><input type="text" className={formStyles.inputfield} placeholder="Enter your username..."/>
           <p className={formStyles.formtext}>Email: </p><input type="text" className={formStyles.inputfield} placeholder="Enter your email..."/>
           <p className={formStyles.formtext}>Password: </p><input type="password" className={formStyles.inputfield} placeholder="Enter your password..."/>
-          <input type="submit" className={formStyles.btn} value="Submit"/>
+          <input type="submit" className={formStyles.btn} value="Sign Up" onSubmit={onSignUp}/>
         </form>
       </div>
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch('http://localhost:5000/credentials')
+  const accounts = await res.json()
+
+  return {
+    props: {
+      accounts
+    }
+  }
 }
